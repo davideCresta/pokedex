47d2fb591beacc0ced6d724683aa0a6df4322d65
davide.cresta <cresta.davide02@gmail.com>
2023-09-26 11:47:28 +0200

    deploy phase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,10 @@ export class AppComponent implements OnInit {
   constructor(private pokedexService: PokedexService) { }
 
   ngOnInit() {
+    this.loadPokedex()
+  };
+
+  loadPokedex() {
     this.pokedexService.getPokemon().subscribe(
       (data: any) => {
         this.pokemonList = data.results;
@@ -22,7 +26,30 @@ export class AppComponent implements OnInit {
       (error) => {
         console.error('Errore nella chiamata all\'API:', error);
       }
-    );
-  }
+    )
+  };
+
+  loadNext() {
+    this.pokedexService.nextPage().subscribe(
+      (data: any) => {
+        this.pokemonList = data.results;
+      },
+      (error) => {
+        console.error('Errore nella chiamata all\'API:', error);
+      }
+    )
+  };
+
+  loadPrevious() {
+    this.pokedexService.previousPage().subscribe(
+      (data: any) => {
+        this.pokemonList = data.results;
+      },
+      (error) => {
+        console.error('Errore nella chiamata all\'API:', error);
+      }
+    )
+  };
+
 }
 
--- src/services/pokedex.service.ts
+++ src/services/pokedex.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -7,11 +8,23 @@ import { HttpClient } from '@angular/common/http';
 export class PokedexService {
 
   private pokeUrl = 'https://pokeapi.co/api/v2/pokemon';
+  private currentPage = 0;
 
   constructor(private http: HttpClient) { }
 
-  getPokemon() {
-    return this.http.get(`${this.pokeUrl}?limit=5`)
+  getPokemon(): Observable<any> {
+    return this.http.get(`${this.pokeUrl}?offset=${this.currentPage}&limit=6`)
   }
 
+  nextPage(){
+    this.currentPage += 6;
+    return this.getPokemon()
+  }
+
+  previousPage(){
+    if (this.currentPage > 0) {
+      this.currentPage -= 6;
+    }
+    return this.getPokemon()
+  }
 }
